700af18270eb10a64ca7b75d287a4fee3a8e14ce
jzw <zhengwei.jin@gmail.com>
2012-07-21 10:07:00 +0800

    完成drip.map的编写

--- server/releng/com.zizibujuan.drip.server.releng/builder/script/masterBuild.sh
+++ server/releng/com.zizibujuan.drip.server.releng/builder/script/masterBuild.sh
@@ -26,6 +26,7 @@ fetchTag=""
 publish=""
 javase170=""
 
+
 updateBaseBuilder () {
     pushd $supportDir
     if [[ ! -d org.eclipse.releng.basebuilder_${basebuilderBranch} ]]; then
@@ -65,14 +66,61 @@ updateRelengProject(){
 }
 
 setProperties(){
-	echo 'setProperties'
+	buildDirectory=$writableBuldRoot/$buildType$timestamp
+	buildLabel=$buildType$date-$time
+	javaHome=/usr/lib64/jvm/java-7-oracle
+	
+	pushd $supportDir
+	launcherJar=$supportDir/$( find org.eclipse.releng.basebuilder/ -name "org.eclipse.equinox.launcher_*.jar" | sort | head -1 )
+	popd
+		
+	#Properties for compilation boot classpaths
+	JAVA70_HOME=/usr/lib64/jvm/java-7-oracle
+
+	javase170="$JAVA70_HOME/jre/lib/resources.jar:$JAVA70_HOME/jre/lib/rt.jar:$JAVA70_HOME/jre/lib/jsse.jar:$JAVA70_HOME/jre/lib/jce.jar:$JAVA70_HOME/jre/lib/charsets.jar"
 }
 
 runBuild(){
-	cmd="java -version"
+cmd="$javaHome/bin/java -enableassertions -jar $launcherJar \
+			-application org.eclipse.ant.core.antRunner \
+			-buildfile $builderDir/buildWebIDE.xml \
+			-Dbuilder=$builderDir/builder \
+			-Dbase=$writableBuildRoot \
+			-DbuildType=$buildType -Dtimestamp=$timestamp -DbuildLabel=$buildLabel \
+			-DgitUser=$user \
+			$tagMaps $compareMaps $fetchTag $publish \
+			-DJavaSE-1.7=$javase170"
+			
 	echo "[`date +%H\:%M\:%S`] Launching Build"
 	$cmd
 	echo "[`date +%H\:%M\:%S`] Build Complete"
+	
+	#stop now if the build failed
+	failure=$(sed -n '/BUILD FAILED/,/Total time/p' $writableBuildRoot/logs/current.log)
+	if [[ ! -z $failure ]]; then
+		compileMsg=""
+		prereqMsg=""
+		pushd $buildDirectory/plugins
+		compileProblems=$( find . -name compilation.problem | cut -d/ -f2 )
+		popd
+		
+		if [[ ! -z $compileProblems ]]; then
+			compileMsg="Compile errors occurred in the following bundles:"
+		fi
+		if [[ -e $buildDirectory/prereqErrors.log ]]; then
+			prereqMsg=`cat $buildDirectory/prereqErrors.log` 
+		fi
+		
+		mailx -s "Orion Build : $buildLabel failed" $resultsEmail <<EOF
+$compileMsg
+$compileProblems
+
+$prereqMsg
+
+$failure
+EOF
+		exit
+	fi
 }
 
 updateRelengProject
