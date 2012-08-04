157d6675644f56b544cdf5d689121709905a9752
jzw <zhengwei.jin@gmail.com>
2012-08-04 12:29:16 +0800

    重命名dao实现类的项目名impl --> mysql

---
+++ server/bundles/com.zizibujuan.drip.server.dao.mysql/src/com/zizibujuan/drip/server/dao/mysql/ExerciseDaoImpl.java
@@ -0,0 +1,22 @@
+package com.zizibujuan.drip.server.dao.mysql;
+
+import java.util.List;
+import java.util.Map;
+
+import com.zizibujuan.drip.server.dao.ExerciseDao;
+import com.zizibujuan.drip.server.util.dao.AbstractDao;
+
+/**
+ * 维护习题 数据访问实现类
+ * @author jinzw
+ * @since 0.0.1
+ */
+public class ExerciseDaoImpl extends AbstractDao implements ExerciseDao {
+
+	@Override
+	public List<Map<String, Object>> get() {
+		
+		return null;
+	}
+
+}
