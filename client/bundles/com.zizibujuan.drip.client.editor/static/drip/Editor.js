56ddcbf64bea57a1907a55dbd1626b5d5ff0554f
jzw <zhengwei.jin@gmail.com>
2012-08-28 23:17:41 +0800

    增加测试浏览器的支持的鼠标和键盘事件。

--- client/bundles/com.zizibujuan.drip.client.editor/static/drip/Editor.js
+++ client/bundles/com.zizibujuan.drip.client.editor/static/drip/Editor.js
@@ -1,12 +1,40 @@
 define(["dojo/_base/declare",
-         "dijit/_WidgetBase"],function(
+        "dojo/on",
+        "dojo/_base/event",
+        "dijit/_WidgetBase",
+        "dojo/dom-construct"],function(
         		 declare,
-        		 _WidgetBase){
+        		 on,
+        		 event,
+        		 _WidgetBase,
+        		 domConstruct){
 	
 	return declare("drip.Editor",[_WidgetBase],{
 		
 		postCreate : function(){
-			this.domNode.innerHTML = "I'm editor";
+			// 创建一个文本框
+			var editorDiv = domConstruct.create("div",{style:{height:"100%"}}, this.domNode);
+			var textarea = domConstruct.create("textarea",null, this.domNode);
+			
+			on(editorDiv, "mousedown",function(e){
+				setTimeout(function() {
+					 textarea.focus();
+			    });
+			});
+			
+			// chrome
+			on(textarea, "textInput", function(e){
+				var original = editorDiv.innerHTML;
+				editorDiv.innerHTML = original + e.data;
+				setTimeout(function() {
+					textarea.value = "";
+			    });
+			});
+			
+			// firefox
+			on(textarea, "input", function(e){
+				console.log(e);
+			});
 		}
 		
 	});
