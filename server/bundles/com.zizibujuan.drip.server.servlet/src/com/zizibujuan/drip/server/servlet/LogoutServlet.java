52ebccbaf31beca5fc4fb509799822f6d5602982
jzw <zhengwei.jin@gmail.com>
2012-09-02 18:00:45 +0800

    服务器端：增加用户注销功能

--- server/bundles/com.zizibujuan.drip.server.servlet/src/com/zizibujuan/drip/server/servlet/LogoutServlet.java
+++ server/bundles/com.zizibujuan.drip.server.servlet/src/com/zizibujuan/drip/server/servlet/LogoutServlet.java
@@ -1,5 +1,11 @@
 package com.zizibujuan.drip.server.servlet;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.zizibujuan.drip.server.util.servlet.DripServlet;
 
 /**
@@ -10,5 +16,20 @@ import com.zizibujuan.drip.server.util.servlet.DripServlet;
 public class LogoutServlet extends DripServlet {
 
 	private static final long serialVersionUID = -4415570541175504883L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		traceRequest(req);
+		String pathInfo = req.getPathInfo();
+		if(isNullOrSeparator(pathInfo)){
+			req.getSession().invalidate();
+			// 注销之后，跳转到首页。
+			// FIXME：要是可以停留在当时的页面，但是显示适合该页面的不同内容，会不会更好些呢？
+			resp.sendRedirect("/");
+			return;
+		}
+		super.doPost(req, resp);
+	}
 
 }
