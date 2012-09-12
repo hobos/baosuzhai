1590b3e311b64599ac758380e37802e18e2d3967
jzw <zhengwei.jin@gmail.com>
2012-09-12 20:35:12 +0800

    在个人首页中显示习题列表

--- client/bundles/com.zizibujuan.drip.client.editor/static/drip/Model.js
+++ client/bundles/com.zizibujuan.drip.client.editor/static/drip/Model.js
@@ -15,10 +15,28 @@ define([ "dojo/_base/declare",
 
 	return declare("drip.Model",null,{
 	
-		doc : null,
+		doc: null,
 		
-		// 当前节点所在的位置
-		position : {node:null, offset : -1},
+		// summary:
+		//		当前光标所在的位置。
+		//		lineIndex和nodeIndex只定位到text和math节点。FIXME：math中的内容不再跟踪？
+		//		还是使用xPath Axes(轴)来定位好一些。
+		//		在一个数组中pop或push每个节点的名称，然后形成一个xpath，然后根据xpath定位到该节点。
+		//		又能如何呢？还是无法将model与view中的位置相对应。
+		// node:
+		//		光标所在的节点
+		// offset：
+		//		光标在node节点中的偏移量，主要是node的子节点或者文本节点内容的偏移量
+		// lineIndex：
+		//		附加信息，主要用于定位node。
+		//		node节点在doc中行数
+		// nodeIndex：
+		//		附加信息，主要用于定位node。
+		//		node在lineIndex指定的行中的索引
+		cursorPosition: {node:null, offset : -1, lineIndex:0, nodeIndex:0},
+		
+		// 当前节点在xml文件中的具体路径
+		path: [],
 		
 		// 调用顺序
 		//		如果是新建，则直接new即可
@@ -28,18 +46,20 @@ define([ "dojo/_base/declare",
 		constructor: function(options){
 			lang.mixin(this, options);
 			this.doc = xmlParser.parse("<root><line></line></root>");
-			this.position.node = this.doc.documentElement.firstChild;
-			this.position.offset = 0;
+			
+			this.cursorPosition.node = this.doc.documentElement.firstChild;
+			this.cursorPosition.offset = 0;
+			
 		},
 		
 		// 如果没有内容，则创建一个新行
 		// 如果存在内容，则加载内容，并将光标移到最后
-		loadData : function(xmlString){
+		loadData: function(xmlString){
 			
 		},
 		
-		setData : function(data){
-			var node = this.position.node;
+		setData: function(data){
+			var node = this.cursorPosition.node;
 			
 			var xmlDoc = this.doc;
 			
@@ -47,34 +67,34 @@ define([ "dojo/_base/declare",
 				var textSpanNode = xmlDoc.createElement("text");
 				node.appendChild(textSpanNode);
 				
-				this.position.node = textSpanNode;
-				this.position.offset = 0;
+				this.cursorPosition.node = textSpanNode;
+				this.cursorPosition.offset = 0;
 			}
 			
-			var offset = this.position.offset;
-			var oldText = this.position.node.textContent;
+			var offset = this.cursorPosition.offset;
+			var oldText = this.cursorPosition.node.textContent;
 			var newText = dripString.insertAtOffset(oldText, offset, data);
 			
-			this.position.offset += data.length;
-			this.position.node.textContent = newText;
+			this.cursorPosition.offset += data.length;
+			this.cursorPosition.node.textContent = newText;
 			
 			this.onChange(data);
 		},
 		
 		// 获取xml文件的字符串值
-		getData : function(){
+		getData: function(){
 			return xmlParser.innerXML(this.doc);
 		},
 		
 		// 习题 line 获取html格式的数据
 		//		展示页面时使用
-		getHTML : function(){
+		getHTML: function(){
 			return dataUtil.xmlDocToHtml(this.doc);
 		},
 		
 		
 		
-		onChange : function(data){
+		onChange: function(data){
 			// 什么也不做，View在该方法执行完毕后，执行刷新操作
 		}
 	
---
+++ client/bundles/com.zizibujuan.drip.client.editor/static/drip/tests/Model.js
@@ -0,0 +1,12 @@
+define([ "doh","drip/Model" ], function(doh,Model) {
+	
+	/*
+	doh.register("dataUtil",[
+		function testXmlStringToHtml(t){
+			var xmlString = "<root><line><text>a</text><math><mn>1</mn></math></line></root>";
+			var expectHtml = "<div><span>a</span><math><mn>1</mn></math></div>";
+			t.is(expectHtml, dataUtil.xmlStringToHtml(xmlString));
+		}
+	]);
+	*/
+});
