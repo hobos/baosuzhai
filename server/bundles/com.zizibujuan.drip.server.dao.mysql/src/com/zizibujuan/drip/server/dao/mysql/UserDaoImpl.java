6ef11b774ea0a2c61f0fe94c14c49bc5ae6f1e59
jzw <zhengwei.jin@gmail.com>
2012-10-30 21:27:37 +0800

    修改表名:DRIP_USER --> DRIP_USER_INFO

--- server/bundles/com.zizibujuan.drip.server.dao.mysql/src/com/zizibujuan/drip/server/dao/mysql/UserDaoImpl.java
+++ server/bundles/com.zizibujuan.drip.server.dao.mysql/src/com/zizibujuan/drip/server/dao/mysql/UserDaoImpl.java
@@ -13,7 +13,7 @@ import com.zizibujuan.drip.server.util.dao.DatabaseUtil;
  */
 public class UserDaoImpl extends AbstractDao implements UserDao {
 
-	private static final String SQL_INSERT_USER = "INSERT INTO DRIP_USER " +
+	private static final String SQL_INSERT_USER = "INSERT INTO DRIP_USER_INFO " +
 			"(LOGIN_NM,LOGIN_EMAIL,LOGIN_PWD,MOBILE,REAL_NM,CRT_TM) " +
 			"VALUES " +
 			"(?,?,?,?,?,now())";
@@ -30,13 +30,13 @@ public class UserDaoImpl extends AbstractDao implements UserDao {
 
 	private static final String SQL_GET_USER = "SELECT " +
 			"DBID,LOGIN_NM,LOGIN_EMAIL,LOGIN_PWD,MOBILE,REAL_NM,CRT_TM " +
-			"FROM DRIP_USER WHERE LOGIN_EMAIL = ? AND LOGIN_PWD = ?";
+			"FROM DRIP_USER_INFO WHERE LOGIN_EMAIL = ? AND LOGIN_PWD = ?";
 	@Override
 	public Map<String, Object> get(String email, String md5Password) {
 		return DatabaseUtil.queryForMap(getDataSource(), SQL_GET_USER, email, md5Password);
 	}
 	
-	private static final String SQL_UPDATE_USER = "UPDATE DRIP_USER SET LAST_LOGIN_TM = now() " +
+	private static final String SQL_UPDATE_USER = "UPDATE DRIP_USER_INFO SET LAST_LOGIN_TM = now() " +
 			"WHERE DBID=?";
 	@Override
 	public void updateLastLoginTime(Long userId) {
@@ -49,7 +49,7 @@ public class UserDaoImpl extends AbstractDao implements UserDao {
 		return DatabaseUtil.queryForMap(getDataSource(), SQL_GET_LOGIN, userId);
 	}
 	
-	private static final String SQL_EMAIL_EXIST = "select 1 from DRIP_USER where LOGIN_EMAIL = ? limit 1";
+	private static final String SQL_EMAIL_EXIST = "select 1 from DRIP_USER_INFO where LOGIN_EMAIL = ? limit 1";
 	@Override
 	public boolean emailIsExist(String email) {
 		String result = DatabaseUtil.queryForString(getDataSource(), SQL_EMAIL_EXIST, email);
